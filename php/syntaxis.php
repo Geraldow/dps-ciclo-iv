9598051a9010bd853891a2b39cb2edab922de5eb
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-06-13 10:08:46 -0500

    add: explicación de sintaxis en php

---
+++ php/syntaxis.php
@@ -0,0 +1,14 @@
+<?php
+
+// Variables 
+$name = "Geraldo";
+$district = "Jaramillo";
+
+// Print 
+echo "Hola, mi nombre " .$name. " y vivo en " .$district. "\n";
+echo "Su nombre es " .$name. " y vive en " .$district. "\n";
+
+// Constants
+define("age", 20);
+echo "Tengo " .age. " años";
+?>
