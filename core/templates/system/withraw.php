48e226b28eef7aa0b0d0d472a20f8fe8c7401607
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-06-20 10:04:36 -0500

    add: se agrega estilos para retiro

--- core/templates/system/withraw.php
+++ core/templates/system/withraw.php
@@ -32,9 +32,26 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/812c8ee19a.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@700&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/styles_withraw.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
-<h1>Module of Withraw</h1>
-<form action="withraw.php" method='POST'>
+<h1 class="title-form">Module of Withraw</h1>
+<form action="withraw.php" method='POST' >
     <label for="number">Amount</label>
     <input type="number" name="amount" id="amount" required>
     <br><br>
@@ -43,6 +60,6 @@ if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
         <option value="pen">PEN</option>
         <option value="usd">USD</option>
     </select>
-    <input type="submit" value="Make withraw">
+    <input type="submit" value="Make withraw" class="boton">
+    <a href="../home.php" class="boton-2">Back</a>
 </form>
-<a href="../home.php">Back</a>
